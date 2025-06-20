8cbdcf60e83b16ec9d04dbee6d2f1a8f0afbd7dd
Alex Kolch <alexkolch.ios@gmail.com>
2025-06-20 12:54:29 +0300

    Initial Commit

---
+++ Unsplash_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Unsplash_test
+//
+//  Created by Алексей Колыченков on 20.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
